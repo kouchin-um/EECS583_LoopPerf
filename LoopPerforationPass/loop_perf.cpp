e04e19158b57e9c2c8d4bd9ee4d1fab99c3831b4
Chen Huang <kouchin@eecs583a.eecs.umich.edu>
2024-04-10 14:39:09 -0400

    count total instr frequencies in the loops

--- LoopPerforationPass/loop_perf.cpp
+++ LoopPerforationPass/loop_perf.cpp
@@ -1,7 +1,19 @@
+#include "llvm/Analysis/BlockFrequencyInfo.h"
+#include "llvm/Analysis/BranchProbabilityInfo.h"
+#include "llvm/Analysis/LoopInfo.h"
+#include "llvm/Analysis/LoopIterator.h"
+#include "llvm/Analysis/LoopPass.h"
 #include "llvm/IR/PassManager.h"
+#include "llvm/IR/LLVMContext.h"
 #include "llvm/Passes/PassBuilder.h"
 #include "llvm/Passes/PassPlugin.h"
 #include "llvm/Support/raw_ostream.h"
+#include "llvm/Support/Format.h"
+#include "llvm/IR/Instruction.h"
+#include "llvm/IR/BasicBlock.h"
+#include "llvm/Transforms/Scalar/LoopPassManager.h"
+#include "llvm/Transforms/Utils/BasicBlockUtils.h"
+#include "llvm/Transforms/Utils/LoopUtils.h"
 
 #include  <iostream>
 
@@ -11,9 +23,31 @@ namespace {
 
 struct LoopPerforationPass : public PassInfoMixin<LoopPerforationPass> {
 
-  PreservedAnalyses run(Function &F, FunctionAnalysisManager &FAM) {
+  typedef struct LoopInfoT {
+        Loop* loop;
+        uint64_t instr_count;
+  } LoopInfo;
 
-    std::cout << "Hello function: " << F.getName().str() << std::endl;
+  PreservedAnalyses run(Function &F, FunctionAnalysisManager &FAM) {
+    llvm::BlockFrequencyAnalysis::Result &bfi = FAM.getResult<BlockFrequencyAnalysis>(F);
+    llvm::LoopAnalysis::Result &li = FAM.getResult<LoopAnalysis>(F);
+
+    // Find the loops that have a lot of iterations and instructions
+    std::vector<LoopInfo> loop_info;
+
+    for (auto &loop : li.getTopLevelLoopsVector()) {
+      uint64_t instr_count = 0;
+      for (auto &bb : loop->getBlocksVector()) {
+        uint64_t bb_freq = bfi.getBlockFreq(bb).getFrequency();
+        uint64_t num_instr = std::distance(bb->begin(), bb->end());
+        instr_count += bb_freq * num_instr;
+      }
+      loop_info.emplace_back(LoopInfo{loop, instr_count});
+    }
+
+    for (auto &loop : loop_info) {
+      errs() << format("%d\n", loop.instr_count);  
+    }
     return PreservedAnalyses::all();
   }
 };
@@ -26,7 +60,7 @@ extern "C" ::llvm::PassPluginLibraryInfo LLVM_ATTRIBUTE_WEAK llvmGetPassPluginIn
       PB.registerPipelineParsingCallback(
         [](StringRef Name, FunctionPassManager &FPM,
         ArrayRef<PassBuilder::PipelineElement>) {
-          if(Name == "func-name"){
+          if(Name == "loop-perf"){
             FPM.addPass(LoopPerforationPass());
             return true;
           }
---
+++ README.md
@@ -0,0 +1,15 @@
+# Build
+
+```
+mkdir build
+cd build
+cmake ..
+make
+```
+
+# Run
+Go to the project directory.
+```
+clang -emit-llvm -S test.c -Xclang -disable-O0-optnone -o test.ll
+opt -disable-output -load-pass-plugin=./build/LoopPerforationPass/LoopPerforationPass.so -passes="loop-perf" test.ll
+```
